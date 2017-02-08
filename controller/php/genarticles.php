a42536b342fd94e1e6578e5e92770d623c35c676
Unknown <quangnhanbui@gmail.com>
2017-02-08 16:30:23 +0100

    add modal

--- controller/php/genarticles.php
+++ controller/php/genarticles.php
@@ -1,31 +1,4 @@
 <?php
 
-ini_set('display_errors', 1);
-ini_set('dispaly_startup_errors', 1);
 
-include 'PDO.php';
-
-$sql = "SELECT `pseudo`,`titre`,`contenu`  FROM `Auteurs`,`Articles`";
-$query = mysqli_query($dbh, $sql);
-
-while ($r = mysqli_fetch_assoc($query)) {
-
-$title = htmlentities($r['titre']); //done to escape html chars
-$des = htmlentities($r['contenu']);
-$nick = htmlentities($r['pseudo']);
-echo "<div id='title' class='text-left'><h4><b>";
-echo $title;
-echo "</b></h4><hr style='border-top: 5px solid #fd6e05; width: 400px; margin-left: 0em;'></div>";
-echo "<div id='des' class='text-left'><i><p>";
-
-echo $des;
-echo "<br>";
-echo "<br>";
-echo "by $nick";
-echo "</i></p></div>";
-echo "<br/>
-<br/>";
-
-
-}
 ?>
--- index.php
+++ index.php
@@ -5,8 +5,11 @@
         <title>mini-blog</title>
         <link rel="stylesheet" type="text/css" rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <link rel="stylesheet" type="text/css" href="view/css/style.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     </head>
     <body>
+
         <h2>blog<a href="">HELLOOOOO</a> </h2>
 
         <div class="row" id="div_modal">
@@ -19,9 +22,41 @@
                                 <div class="gen col-xs-11 col-xs-offset-1" id="<?= $row['id_articles'] ?>">
                                     <div class="titre"><b>Titre :</b> <?php echo $row["titre"]."<br>";?></div>
                                     <div class="pseudo"><b>Pseudo :</b> <?php echo $row["pseudo"]."<br>";?></div>
-                                    <a class="appa_art" href="index.php?param_url=<?= $row['id_articles'] ?>" >
-                                        <button type="button" class="btn btn-default col-xs-1" onclick="popup()">voir article</button>
+                                    <!-- <a class="appa_art" href="index.php?param_url=<?= $row['id_articles'] ?>" > -->
+                                        <button type="button" class="btn btn-default col-xs-1" id="myBtn">voir article</button>
+                                        <!-- Modal -->
+  <div class="modal fade" id="myModal" role="dialog">
+    <div class="modal-dialog">
+
+      <!-- Modal content-->
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h4 class="modal-title">Modal Header</h4>
+        </div>
+        <div class="modal-body">
+          <p>Some text in the modal.</p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        </div>
+      </div>
+
+    </div>
+  </div>
+
+</div>
+
+<script>
+$(document).ready(function(){
+    $("#myBtn").click(function(){
+        $("#myModal").modal();
+    });
+});
+</script>
+
                                         <?php echo "<br><br>"?>
+
                                     </a>
                                 </div>
 
